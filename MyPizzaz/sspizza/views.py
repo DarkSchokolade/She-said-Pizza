08fc6b4a1dc200a876958d3fb95d6b03870df670
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-08-19 16:13:50 +0530

    initial pizza on dashboard

--- MyPizzaz/sspizza/views.py
+++ MyPizzaz/sspizza/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+from .models import *
 
 def home(request):
-    return render(request, 'sspizza/dashboard.html')
+    pizzas = Pizza.objects.all()
+    context = {'pizzas': pizzas}
+    return render(request, 'sspizza/dashboard.html', context)
     # return HttpResponse('Welcome to the development of She Said Pizza')
