db594fd9d461b7dcc9aadb5aba5bb8ba32e044ac
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-08-27 19:15:53 +0530

    initial login and register page

--- MyPizzaz/sspizza/urls.py
+++ MyPizzaz/sspizza/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'sspizza'
 urlpatterns = [
+    path('register/', views.RegisterPage, name='register'),
+    path('login/', views.LoginPage, name='login'),
     path('', views.home, name='home'),
 ]
--- MyPizzaz/sspizza/views.py
+++ MyPizzaz/sspizza/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 # Create your views here.
 from .models import *
+from .forms import *
 
 def home(request):
     pizzas = Pizza.objects.all()
     context = {'pizzas': pizzas}
     return render(request, 'sspizza/dashboard.html', context)
     # return HttpResponse('Welcome to the development of She Said Pizza')
+
+def RegisterPage(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('sspizza:login')
+    context = {'form': form}
+    return render(request, 'sspizza/register.html', context)
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('sspizza:home')
+        else:
+            messages.info(request, 'Password or Username must be incorrect')
+    return render(request, 'sspizza/login.html')
