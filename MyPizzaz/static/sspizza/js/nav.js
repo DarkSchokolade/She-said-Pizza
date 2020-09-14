11314844a8e1db71d97fd15b0b5a1af6f2502d71
Suhas K Shetty <57042304+DarkSchokolade@users.noreply.github.com>
2020-09-14 13:50:41 +0530

    Added Responsive NavBar #9

---
+++ MyPizzaz/static/sspizza/js/nav.js
@@ -0,0 +1,23 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li')
+
+    burger.addEventListener('click', () => {
+        // Toggel Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.4}s`
+            }
+        })
+        // burger animation
+        burger.classList.toggle('toggle');
+    })
+}
+
+navSlide();
